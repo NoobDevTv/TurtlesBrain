81e81ba66cfb489efac30bb919949280bd67c0ed
MarcusAurelius <Marcus.Aurelius@gallimathias.de>
2016-04-25 21:33:39 +0200

    Delete the Redcorona.dll and some Format changes

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,12 +12,11 @@ namespace Client
         static void Main(string[] args)
         {
             Console.ReadKey();
-            var server = Server.Connect("localhost", 7777, "susch","riechtstreng");
+            Server.Connect("localhost", 7777, "susch","riechtstreng");
             Console.ReadKey();
-            Turtle turtle = server["suschstinkt"];
-            string o;
-            Console.WriteLine(turtle.forward(out o));
-            Console.WriteLine(o);
+            Turtle turtle = new Turtle("suschrichtstark");
+            Turtle turtle2 = new Turtle("fischNamenssuschi");
+            Console.WriteLine(turtle.forward());
             //SkyWriter writer = new SkyWriter(
             //    new string[] { "turtleName1", "turtleName2", "turtleName3", "turtleName4", "turtleName5", }, 
             //    "text to write","turtle","b");
